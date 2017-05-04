879de5877b08d7bfa177101e7a6d4e7c5703e5d7
Ilia Vinokurov <ilvin@mail.ru>
2017-05-04 19:31:51 +0300

    Added boost unit tests (one), code refactoring

--- libraries/MTLoop/MTLoop.h
+++ libraries/MTLoop/MTLoop.h
@@ -6,106 +6,121 @@
 #define MTLOOP_H
 
 #include <inttypes.h>
+#include <memory>
 #include <vector>
 #include <string>
 
+#include <iostream>
+
 namespace MT {
 
-    class TStat {
-    private:
-        uint32_t counter = 0;
-        uint32_t totalDuration = 0;
-        uint32_t startTime = 0;
-        uint32_t stopTime = 0;
-        uint32_t maxDuration = 0;
-    public:
-        TStat() {}
+    class TTimer {
 
-        void Start() {
-            startTime = 0; //micros();
-        }
+        private:
+            uint32_t curTime = 0;
 
-        void Stop() {
-            stopTime = 0; //micros();
-            uint32_t d = GetDuration();
-            if (d > maxDuration)
-                maxDuration = d;
-            totalDuration += d;
-            counter++;
-        }
-
-        uint32_t GetDuration() {
-            return stopTime - startTime;
-        }
-
-        uint32_t GetAvgDuration() {
-            if (counter == 0)
-                return 0;
-            return totalDuration / counter;
-        }
-
-        uint32_t GetMaxDuration() {
-            return maxDuration;
-        }
-
-        uint32_t GetCounter() {
-            return counter;
-        }
-
-        uint32_t GetStartTime() {
-            return startTime;
-        }
-
-        uint32_t GetStopTime() {
-            return stopTime;
-        }
+        public:
+            virtual uint32_t GetTime() {
+                return ++curTime;
+            }
     };
 
+    TTimer* defaultTimer = new TTimer();
+
     class TLog {
+
         private:
+
         public:
             TLog() {}
             void Log(std::string logLine = "") {
             }
     };
 
-    class TTask {
+    TLog* defaultLog = new TLog();
+
+    class TStat {
+
         private:
+            uint32_t startTime = 0;
+            uint32_t stopTime = 0;
+
         public:
-            TTask() {}
+            inline void Start(TTimer& timer) {
+                startTime = timer.GetTime();
+            }
+
+            inline void Stop(TTimer& timer) {
+                stopTime = timer.GetTime();
+            }
+
+            inline uint32_t GetDuration() {
+                return stopTime - startTime;
+            }
+
+            inline uint32_t GetStartTime() {
+                return startTime;
+            }
+
+            inline uint32_t GetStopTime() {
+                return stopTime;
+            }
+    };
+
+    class TTask {
+
+        private:
+            std::string name;
+
+        public:
+            TTask(std::string name): name(name) {}
             virtual ~TTask() {}
-            virtual void Run() = 0;
+            inline const std::string& GetName() {
+                return name;
+            }
+            virtual void Run(TLog& log) = 0;
     };
 
     class TTimeSlot {
     public:
-        TStat stat;
-        TLog log;
+        TStat taskStat;
 
     private:
-        TTask* task;
+        std::shared_ptr<TTask> task;
         uint32_t startTime;
         uint32_t duration;
 
     public:
-        TTimeSlot(TTask* task, uint32_t duration = 100)
+        TTimeSlot(std::shared_ptr<TTask> task, uint32_t duration = 100)
             : task(task)
-            , startTime(0)
+            , startTime(1)
             , duration(duration) {}
 
         ~TTimeSlot() {
-            delete task;
+//            delete task;
         }
 
-        void Start(uint32_t currTime) {
-            startTime = currTime;
-            stat.Start();
-            task->Run();
-            stat.Stop();
+        inline void SetStartTime(uint32_t time) {
+            startTime = time;
         }
 
-        bool IsStoped(uint32_t currTime) {
-            return currTime >= startTime + duration;
+        void Tick(TTimer& timer, TLog& log) {
+            std::cout
+                << "TIME=" << timer.GetTime()
+                << "; startTime=" << startTime
+                << "; duration=" << duration
+                << std::endl;
+            if (timer.GetTime() <= startTime + duration) { // Мы находимся в слоте
+            std::cout
+                << "taskStartTime=" << taskStat.GetStartTime()
+                << "; startTime=" << startTime
+                << std::endl;
+                if (taskStat.GetStartTime() < startTime) { // Задача еще не была запущена в этом слоте
+                    taskStat.Start(timer);
+                    task->Run(log);
+                    taskStat.Stop(timer);
+                }
+            }
         }
     };
 
@@ -123,9 +138,15 @@ namespace MT {
 
     class TLoop {
         private:
+            TTimer* timer;
+            TLog* log;
             std::vector<TTimeSlotChain> timeSlotChains;
         public:
-            TLoop() {}
+            TLoop(TTimer* timer = defaultTimer, TLog* log = defaultLog)
+                : timer(timer)
+                , log(log)
+            {}
+
             void AddTaskChain(std::vector<TTimeSlot> timeSlots) {
                 timeSlotChains.push_back(TTimeSlotChain(timeSlots));
             }
--- src/MTLoop_ut.cpp
+++ src/MTLoop_ut.cpp
@@ -1,16 +1,50 @@
 #define BOOST_TEST_MODULE testMTLoop
-#include <boost/test/unit_test.hpp>
+//#include <boost/test/unit_test.hpp>
+#include <boost/test/included/unit_test.hpp>
 #include "MTLoop.h"
 #include <string>
+#include <memory>
 
 
 using namespace MT;
 
+class TMockTask: public TTask {
+    public:
+    bool taskIsRun = false;
+    TMockTask(std::string name): TTask(name) {}
+    void Run(TLog& log) {
+        std::cout << "RUN!!!" << std::endl;
+        taskIsRun = true;
+    }
+};
+
 BOOST_AUTO_TEST_SUITE(testSuiteMTLoop)
 
-    BOOST_AUTO_TEST_CASE( testMTLoop ) {
+    struct TTaskFixture {
+
+        std::shared_ptr<TLog> log;
+        std::shared_ptr<TTimer> timer;
+        std::shared_ptr<TMockTask> myTask;
+
+        TTaskFixture() {
+            log = std::shared_ptr<TLog>(new TLog);
+            timer = std::shared_ptr<TTimer>(new TTimer);
+            myTask = std::shared_ptr<TMockTask>(new TMockTask("TestTask"));
+        }
+       ~TTaskFixture() {
+       }
+    };
+
+
+    BOOST_FIXTURE_TEST_CASE( testTTimeSlot, TTaskFixture ) {
         {
-            BOOST_CHECK_EQUAL(1, 1);
+            TTimeSlot slot(myTask, 100);
+            slot.SetStartTime(1);
+
+            slot.Tick(*timer, *log);
+
+            BOOST_CHECK_EQUAL(myTask->GetName(), "TestTask");
+            BOOST_CHECK_EQUAL(myTask->taskIsRun, true);
         }
     }
 
